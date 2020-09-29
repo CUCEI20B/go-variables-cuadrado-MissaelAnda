95f07b281c844a9214ae438b084b227fdff8ea42
Missael H. Anda <hector.handa@alumnos.udg.mx>
2020-09-29 11:42:41 -0500

    prueba01

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var input uint
+	fmt.Scan(&input)
+	var area = input * input
+	fmt.Println(area)
 }
